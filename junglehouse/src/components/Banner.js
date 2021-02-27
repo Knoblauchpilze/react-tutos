59b771b2c2e990b5d702c84faa1b9746590413a4
Knoblauchpilze <maxime.rigaud.perso@gmail.com>
2021-02-27 15:45:17 +0100

    feat: Working implementation of the Jungle House react app from the part 2 of the tutorial

---
+++ junglehouse/src/components/Banner.js
@@ -0,0 +1,8 @@
+
+function Banner() {
+  return (
+    <h1>The Jungle House</h1>
+  );
+}
+
+export default Banner
---
+++ junglehouse/src/components/Cart.js
@@ -0,0 +1,20 @@
+
+function Cart() {
+  const monsteraPrice = 12;
+  const ivyPrice = 8;
+  const flowerPrice = 15;
+
+  return (
+    <div>
+      <h2>Cart:</h2>
+      <ul>
+        <li>Monstera: {monsteraPrice} euro(s)</li>
+        <li>Ivy: {ivyPrice} euro(s)</li>
+        <li>Flowers: {flowerPrice} euro(s)</li>
+      </ul>
+      <p>Total: {monsteraPrice + ivyPrice + flowerPrice} euro(s)</p>
+    </div>
+  );
+}
+
+export default Cart;
